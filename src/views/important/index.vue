1ed63dd177356f09b954030f6c66594b50b4e749
AjaxSir <745284215@qq.com>
2020-02-16 18:28:54 +0800

    重点人员

---
+++ src/views/important/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span='4'>今日通知</el-col>
+    </el-row>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+<style lang="less" scoped>
+
+</style>
